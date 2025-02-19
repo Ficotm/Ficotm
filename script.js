3dcb8cf577c693f93c27789f90469aafd87e474f
Ficotm <dficod@gmail.com>
2025-02-19 11:00:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav ul li a");
-    links.forEach(link => {
-        link.addEventListener("click", function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-            window.scrollTo({
-                top: targetSection.offsetTop - 50,
-                behavior: "smooth"
-            });
-        });
-    });
-});
+<script>
+    function pozdrav() {
+        alert("Zdravo, dobrodošao na moj sajt!");
+    }
+</script>
+
+<button onclick="pozdrav()">Klikni me</button>
